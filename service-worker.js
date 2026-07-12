2e7a7f775733facddcf76b77cbb3786bc577bfa9
koro <kosuke347dte@gmail.com>
2026-07-12 23:47:55 +0900

    add PWA

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("classjump-cache").then((cache) => {
+      return cache.addAll([
+        "./index.html",
+        "./manifest.json",
+        "./school_96.png",
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
